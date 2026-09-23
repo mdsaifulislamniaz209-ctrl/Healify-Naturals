7230d8292444936b83166dcbb4e776c393566c60
mdsaifulislamniaz209-ctrl <mdsaifulislamniaz209@gmail.com>
2026-09-23 18:20:37 +0600

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {  
   return {
+    base: '/Healify-Naturals/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
